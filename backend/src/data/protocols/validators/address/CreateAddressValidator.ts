32cf4eec309d32a84ebf9c49f5496f3a62abde8d
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-24 17:03:31 -0300

    refactor(backend): update create address validator to return error message

--- backend/src/data/protocols/validators/address/CreateAddressValidator.ts
+++ backend/src/data/protocols/validators/address/CreateAddressValidator.ts
@@ -1,16 +1,29 @@
-import { type Address } from '../../../../domain'
-
 export namespace CreateAddressValidator {
   export type Request = any
 
   type SuccessResponse = {
     success: true
-    data: Address
+    data: {
+      postalCode: string
+      street: string
+      number: string
+      neighborhood: string
+      complement: string | null
+      city: string
+      state: string
+    }
   }
+
+  type FailResponseErrorTypes =
+    'INVALID_PROPERTY_TYPE' |
+    'INVALID_POSTAL_CODE_FORMAT'
 
   type FailResponse = {
     success: false
-    error: string
+    error: {
+      type: FailResponseErrorTypes
+      message?: string
+    }
   }
 
   export type Response = SuccessResponse | FailResponse
