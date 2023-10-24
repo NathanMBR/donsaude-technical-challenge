d90ec80450bf30651c397181e701f9953fb99abc
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-24 19:45:47 -0300

    chore(backend): add zod create address schema

---
+++ backend/src/infra/validators/zod/schemas/address/zodCreateAddressSchema.ts
@@ -0,0 +1,38 @@
+import { z as zod } from 'zod'
+
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+export const zodCreateAddressSchema = zod.object({
+  postalCode: zod.string({
+    description: 'The address postal code',
+    required_error: 'postalCode is required',
+    invalid_type_error: 'postalCode must be a string'
+  }),
+  street: zod.string({
+    description: 'The address street',
+    required_error: 'street is required',
+    invalid_type_error: 'street must be a string'
+  }),
+  number: zod.string({
+    description: 'The address number',
+    required_error: 'number is required',
+    invalid_type_error: 'number must be a string'
+  }),
+  neighborhood: zod.string({
+    description: 'The address neighborhood',
+    required_error: 'neighborhood is required',
+    invalid_type_error: 'neighborhood must be a string'
+  }),
+  complement: zod.string({
+    description: 'The address complement',
+    invalid_type_error: 'complement must be a string'
+  })
+    .nullable()
+    .optional()
+    .transform(complement => complement || null),
+  city: zod.string(),
+  state: zod.string()
+}, {
+  description: 'The address payload',
+  required_error: 'payload is required',
+  invalid_type_error: 'payload must be an object'
+})
