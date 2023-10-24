4632ed18e97e664b96ad9bd5b8e6c80afd427493
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-24 16:38:44 -0300

    chore(backend): add create address implementation

---
+++ backend/src/data/usecases/address/CreateAddressImpl.ts
@@ -0,0 +1,28 @@
+import { type CreateAddress } from '../../../domain'
+import {
+  type CreateAddressValidator,
+  type CreateAddressRepository
+} from '../../protocols'
+
+export class CreateAddressImpl implements CreateAddress {
+  constructor (
+    private readonly createAddressValidator: CreateAddressValidator,
+    private readonly createAddressRepository: CreateAddressRepository
+  ) {}
+
+  async execute (request: CreateAddress.Request): CreateAddress.Response {
+    const validationResult = this.createAddressValidator.validate(request)
+    if (!validationResult.success)
+      return {
+        success: false,
+        error: 'INVALID_REQUEST'
+      }
+
+    const address = await this.createAddressRepository.create(validationResult.data)
+
+    return {
+      success: true,
+      data: address
+    }
+  }
+}
