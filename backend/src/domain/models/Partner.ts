4d8652d3e6cfab7e9735c03726b4dbb4c95e543d
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-24 15:48:23 -0300

    chore(backend): add partner model

---
+++ backend/src/domain/models/Partner.ts
@@ -0,0 +1,16 @@
+export type Partner = {
+  id: number
+  name: string
+  email: string
+  password: string
+  category: string
+  cnpj: string
+  phone: string
+  cellphone: string
+  clinicalManagerName: string
+  financialManagerName: string
+  addressId: number
+  createdAt: Date
+  updatedAt: Date
+  deletedAt: Date | null
+}
