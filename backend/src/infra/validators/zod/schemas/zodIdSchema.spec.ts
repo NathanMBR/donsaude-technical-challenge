086307d19cb433a73771268731ce5bf84b84f11b
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-25 15:11:39 -0300

    test(backend): add error messages to zod id schema

--- backend/src/infra/validators/zod/schemas/zodIdSchema.spec.ts
+++ backend/src/infra/validators/zod/schemas/zodIdSchema.spec.ts
@@ -15,6 +15,7 @@ type SchemaInfer = zod.infer<typeof zodIdSchema>
 type SUTSuccessResponse = SafeParseSuccess<SchemaInfer>
 type SUTFailResponse = SafeParseError<SchemaInfer>
 
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 describe('zodIdSchema', () => {
   it('should successfully validate an id', () => {
     const SUTRequest = 1
@@ -38,34 +39,43 @@ describe('zodIdSchema', () => {
   })
 
   it("should fail validation if id isn't defined", () => {
+    // type coercion makes this test fail if required message is checked, since undefined is coerced to NaN
     const SUTRequest = undefined
 
     const SUTResponse = zodIdSchema.safeParse(SUTRequest) as SUTFailResponse
+    // const issue = SUTResponse.error.issues[0]!
 
     expect(SUTResponse.success).toBe(false)
+    // expect(issue.message).toBe('ID is required')
   })
 
   it("should fail validation if id isn't a number", () => {
     const SUTRequest = 'test'
 
     const SUTResponse = zodIdSchema.safeParse(SUTRequest) as SUTFailResponse
+    const issue = SUTResponse.error.issues[0]!
 
     expect(SUTResponse.success).toBe(false)
+    expect(issue.message).toBe('ID must be a number')
   })
 
   it("should fail validation if id isn't positive", () => {
     const SUTRequest = -1
 
     const SUTResponse = zodIdSchema.safeParse(SUTRequest) as SUTFailResponse
+    const issue = SUTResponse.error.issues[0]!
 
     expect(SUTResponse.success).toBe(false)
+    expect(issue.message).toBe('ID must be a positive number')
   })
 
   it("should fail validation if id isn't an integer", () => {
     const SUTRequest = 3.1415
 
     const SUTResponse = zodIdSchema.safeParse(SUTRequest) as SUTFailResponse
+    const issue = SUTResponse.error.issues[0]!
 
     expect(SUTResponse.success).toBe(false)
+    expect(issue.message).toBe('ID must be an integer')
   })
 })
