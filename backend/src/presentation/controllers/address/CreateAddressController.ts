5e458b2e4b8e33ebe8cec06acbb4d00d40c4cdee
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-24 23:17:04 -0300

    chore(backend): add create address controller

---
+++ backend/src/presentation/controllers/address/CreateAddressController.ts
@@ -0,0 +1,20 @@
+import { type CreateAddress } from '../../../domain'
+import { HttpResponseHelper } from '../../helpers'
+import { type Controller } from '../../protocols'
+
+export class CreateAddressController implements Controller {
+  constructor (private readonly createAddress: CreateAddress) {}
+
+  async handle (request: Controller.Request): Controller.Response {
+    const { body } = request
+
+    const createAddressResponse = await this.createAddress.execute(body)
+    if (createAddressResponse.success)
+      return HttpResponseHelper.created(createAddressResponse.data)
+
+    if (createAddressResponse.error.type === 'INVALID_REQUEST')
+      return HttpResponseHelper.badRequest(createAddressResponse.error.message)
+
+    return HttpResponseHelper.internalServerError()
+  }
+}
