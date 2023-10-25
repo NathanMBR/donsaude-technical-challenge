1e333cf1f71cb05a73f2327c168eff6bd6e2e702
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-25 18:54:49 -0300

    feat(backend): find many addresses

--- backend/src/main/routes/express/expressAddressRoutes.ts
+++ backend/src/main/routes/express/expressAddressRoutes.ts
@@ -2,20 +2,24 @@ import { Router } from 'express'
 
 import {
   makeCreateAddressController,
-  makeFindOneAddressController
+  makeFindOneAddressController,
+  makeFindManyAddressesController
 } from '../../factories'
 import { expressRouteAdapter } from '../../adapters'
 
 const createAddressController = makeCreateAddressController()
 const findOneAddressController = makeFindOneAddressController()
+const findManyAddressesController = makeFindManyAddressesController()
 
 const createAddressRoute = expressRouteAdapter(createAddressController)
 const findOneAddressRoute = expressRouteAdapter(findOneAddressController)
+const findManyAddressesRoute = expressRouteAdapter(findManyAddressesController)
 
 const expressAddressRoutes = Router()
 
 /* eslint-disable @typescript-eslint/no-misused-promises */
 expressAddressRoutes.post('/', createAddressRoute)
 expressAddressRoutes.get('/:id', findOneAddressRoute)
+expressAddressRoutes.get('/', findManyAddressesRoute)
 
 export { expressAddressRoutes }
