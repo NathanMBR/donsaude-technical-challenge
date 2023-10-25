673d9f248aa1c3325f0efb5252c595844da9c0d2
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-25 15:06:52 -0300

    chore(backend): add error messages to zod id schema

--- backend/src/infra/validators/zod/schemas/zodIdSchema.ts
+++ backend/src/infra/validators/zod/schemas/zodIdSchema.ts
@@ -1,5 +1,8 @@
 import { z as zod } from 'zod'
 
-export const zodIdSchema = zod.coerce.number()
-  .positive()
-  .int()
+export const zodIdSchema = zod.coerce.number({
+  description: 'Entity ID',
+  invalid_type_error: 'ID must be a number'
+})
+  .positive('ID must be a positive number')
+  .int('ID must be an integer')
