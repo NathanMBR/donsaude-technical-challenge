6706389f720ece0fd560f55ef78e522c7223dd99
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-25 11:54:21 -0300

    feat(backend): find one address

--- backend/src/main/routes/express/expressAddressRoutes.ts
+++ backend/src/main/routes/express/expressAddressRoutes.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express'
 
-import { makeCreateAddressController } from '../../factories'
+import {
+  makeCreateAddressController,
+  makeFindOneAddressController
+} from '../../factories'
 import { expressRouteAdapter } from '../../adapters'
 
 const createAddressController = makeCreateAddressController()
+const findOneAddressController = makeFindOneAddressController()
 
 const createAddressRoute = expressRouteAdapter(createAddressController)
+const findOneAddressRoute = expressRouteAdapter(findOneAddressController)
 
 const expressAddressRoutes = Router()
 
 /* eslint-disable @typescript-eslint/no-misused-promises */
 expressAddressRoutes.post('/', createAddressRoute)
+expressAddressRoutes.get('/:id', findOneAddressRoute)
 
 export { expressAddressRoutes }
