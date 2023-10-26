3924c3bfad29442bca6a254dc99adc66bdda1b42
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-26 15:57:58 -0300

    feat(backend): find one partner

--- backend/src/main/routes/express/expressPartnerRoutes.ts
+++ backend/src/main/routes/express/expressPartnerRoutes.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express'
 
-import { makeCreatePartnerController } from '../../factories'
+import {
+  makeCreatePartnerController,
+  makeFindOnePartnerController
+} from '../../factories'
 import { expressRouteAdapter } from '../../adapters'
 
 const createPartnerController = makeCreatePartnerController()
+const findOnePartnerController = makeFindOnePartnerController()
 
 const createPartnerRoute = expressRouteAdapter(createPartnerController)
+const findOnePartnerRoute = expressRouteAdapter(findOnePartnerController)
 
 const expressPartnerRoutes = Router()
 
 /* eslint-disable @typescript-eslint/no-misused-promises */
 expressPartnerRoutes.post('/', createPartnerRoute)
+expressPartnerRoutes.get('/:id', findOnePartnerRoute)
 
 export { expressPartnerRoutes }
