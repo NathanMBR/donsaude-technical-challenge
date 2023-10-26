8c5680eb704a4f4cdcec453e4ea36c6fc63061b8
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-26 18:01:23 -0300

    feat(backend): find many partners

--- backend/src/main/routes/express/expressPartnerRoutes.ts
+++ backend/src/main/routes/express/expressPartnerRoutes.ts
@@ -2,20 +2,24 @@ import { Router } from 'express'
 
 import {
   makeCreatePartnerController,
-  makeFindOnePartnerController
+  makeFindOnePartnerController,
+  makeFindManyPartnersController
 } from '../../factories'
 import { expressRouteAdapter } from '../../adapters'
 
 const createPartnerController = makeCreatePartnerController()
 const findOnePartnerController = makeFindOnePartnerController()
+const findManyPartnersController = makeFindManyPartnersController()
 
 const createPartnerRoute = expressRouteAdapter(createPartnerController)
 const findOnePartnerRoute = expressRouteAdapter(findOnePartnerController)
+const findManyPartnersRoute = expressRouteAdapter(findManyPartnersController)
 
 const expressPartnerRoutes = Router()
 
 /* eslint-disable @typescript-eslint/no-misused-promises */
 expressPartnerRoutes.post('/', createPartnerRoute)
 expressPartnerRoutes.get('/:id', findOnePartnerRoute)
+expressPartnerRoutes.get('/', findManyPartnersRoute)
 
 export { expressPartnerRoutes }
