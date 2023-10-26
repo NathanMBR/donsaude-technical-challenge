a71d254977410845723064e381bff2c67d3928ce
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-26 12:05:05 -0300

    chore(backend): add create partner usecase

---
+++ backend/src/domain/usecases/partner/CreatePartner.ts
@@ -0,0 +1,36 @@
+import { type Partner } from '../../models'
+
+export namespace CreatePartner {
+  export type Request = {
+    name: string
+    email: string
+    password: string
+    category: string
+    cnpj: string
+    phone: string
+    cellphone: string
+    clinicalManagerName: string
+    financialManagerName: string
+    addressId: number
+  }
+
+  type SuccessResponse = {
+    type: 'SUCCESS'
+    data: Partner
+  }
+
+  type InvalidRequestResponse = {
+    type: 'INVALID_REQUEST'
+    message?: string
+  }
+
+  type AddressNotFoundResponse = {
+    type: 'ADDRESS_NOT_FOUND'
+  }
+
+  export type Response = Promise<SuccessResponse | InvalidRequestResponse | AddressNotFoundResponse>
+}
+
+export interface CreatePartner {
+  execute(request: CreatePartner.Request): CreatePartner.Response
+}
