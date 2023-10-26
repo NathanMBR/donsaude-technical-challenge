bb356904c45f85259a3a25007a793c7728ee9b21
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-26 14:28:29 -0300

    chore(backend): add create partner controller

---
+++ backend/src/presentation/controllers/partner/CreatePartnerController.ts
@@ -0,0 +1,21 @@
+import { type Controller } from '../../protocols'
+import { type CreatePartner } from '../../../domain'
+import { HttpResponseHelper } from '../../helpers'
+import { type HttpRequest } from '../../models'
+
+export class CreatePartnerController implements Controller {
+  constructor (private readonly createPartner: CreatePartner) {}
+
+  async handle (request: HttpRequest): Controller.Response {
+    const { body } = request
+
+    const createPartnerResponse = await this.createPartner.execute(body)
+    if (createPartnerResponse.type === 'INVALID_REQUEST')
+      return HttpResponseHelper.badRequest(createPartnerResponse.message)
+
+    if (createPartnerResponse.type === 'ADDRESS_NOT_FOUND')
+      return HttpResponseHelper.notFound('Address not found')
+
+    return HttpResponseHelper.created(createPartnerResponse.data)
+  }
+}
