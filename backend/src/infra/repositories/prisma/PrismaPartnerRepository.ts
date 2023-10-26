d753ee22b095b682eb7f5c16861c5fcb4cfdeb2e
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-26 15:42:33 -0300

    chore(backend): add prisma find one partner repository

--- backend/src/infra/repositories/prisma/PrismaPartnerRepository.ts
+++ backend/src/infra/repositories/prisma/PrismaPartnerRepository.ts
@@ -1,8 +1,14 @@
 import { type PrismaClient } from '@prisma/client'
 
-import { type CreatePartnerRepository } from '../../../data'
+import {
+  type CreatePartnerRepository,
+  type FindOnePartnerRepository
+} from '../../../data'
 
-export class PrismaPartnerRepository implements CreatePartnerRepository {
+export class PrismaPartnerRepository implements
+  CreatePartnerRepository,
+  FindOnePartnerRepository
+{
   constructor (
     private readonly prisma: PrismaClient
   ) {}
@@ -10,6 +16,18 @@ export class PrismaPartnerRepository implements CreatePartnerRepository {
   async create (request: CreatePartnerRepository.Request): CreatePartnerRepository.Response {
     const partner = await this.prisma.partner.create({
       data: request
+    })
+
+    return partner
+  }
+
+  async findOne (request: FindOnePartnerRepository.Request): FindOnePartnerRepository.Response {
+    const { id } = request
+
+    const partner = await this.prisma.partner.findUnique({
+      where: {
+        id
+      }
     })
 
     return partner
