27cadad18853038c23a5b7e422aaf27bab71123a
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-26 19:20:58 -0300

    feat(backend): update partner

--- backend/src/main/routes/express/expressPartnerRoutes.ts
+++ backend/src/main/routes/express/expressPartnerRoutes.ts
@@ -3,17 +3,20 @@ import { Router } from 'express'
 import {
   makeCreatePartnerController,
   makeFindOnePartnerController,
-  makeFindManyPartnersController
+  makeFindManyPartnersController,
+  makeUpdatePartnerController
 } from '../../factories'
 import { expressRouteAdapter } from '../../adapters'
 
 const createPartnerController = makeCreatePartnerController()
 const findOnePartnerController = makeFindOnePartnerController()
 const findManyPartnersController = makeFindManyPartnersController()
+const updatePartnerController = makeUpdatePartnerController()
 
 const createPartnerRoute = expressRouteAdapter(createPartnerController)
 const findOnePartnerRoute = expressRouteAdapter(findOnePartnerController)
 const findManyPartnersRoute = expressRouteAdapter(findManyPartnersController)
+const updatePartnerRoute = expressRouteAdapter(updatePartnerController)
 
 const expressPartnerRoutes = Router()
 
@@ -21,5 +24,6 @@ const expressPartnerRoutes = Router()
 expressPartnerRoutes.post('/', createPartnerRoute)
 expressPartnerRoutes.get('/:id', findOnePartnerRoute)
 expressPartnerRoutes.get('/', findManyPartnersRoute)
+expressPartnerRoutes.put('/:id', updatePartnerRoute)
 
 export { expressPartnerRoutes }
