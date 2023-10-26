dc85474c695f2e27dfa29b6ce70b2d0f2ee12b04
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-26 16:43:26 -0300

    chore(backend): add find many partners implementation

---
+++ backend/src/data/usecases/partner/FindManyPartnersImpl.ts
@@ -0,0 +1,44 @@
+import { type FindManyPartners } from '../../../domain'
+import {
+  type PaginationValidator,
+  type FindManyPartnersRepository,
+  type CountManyPartnersRepository
+} from '../../protocols'
+import {
+  getRepositoryPaginationRequest,
+  getRepositoryPaginationResponse
+} from '../../helpers'
+
+export class FindManyPartnersImpl implements FindManyPartners {
+  constructor (
+    private readonly paginationValidator: PaginationValidator,
+    private readonly findManyPartnersRepository: FindManyPartnersRepository,
+    private readonly countManyPartnersRepository: CountManyPartnersRepository
+  ) {}
+
+  async execute (request: FindManyPartners.Request): FindManyPartners.Response {
+    const paginationParams = this.paginationValidator.validate(request)
+
+    const searchParams = getRepositoryPaginationRequest({
+      page: paginationParams.page,
+      quantity: paginationParams.quantity
+    })
+
+    const [partners, partnersCount] = await Promise.all([
+      this.findManyPartnersRepository.findMany({
+        ...searchParams,
+        search: paginationParams.search
+      }),
+      this.countManyPartnersRepository.countMany({ search: paginationParams.search })
+    ])
+
+    const paginatedPartners = getRepositoryPaginationResponse({
+      take: searchParams.take,
+      skip: searchParams.skip,
+      count: partnersCount,
+      data: partners
+    })
+
+    return paginatedPartners
+  }
+}
--- backend/src/data/usecases/partner/index.ts
+++ backend/src/data/usecases/partner/index.ts
@@ -1,2 +1,3 @@
 export * from './CreatePartnerImpl'
 export * from './FindOnePartnerImpl'
+export * from './FindManyPartnersImpl'
