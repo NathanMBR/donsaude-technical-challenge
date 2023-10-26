66bd541824be5096927bb0f51eb996b6590605e1
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-26 17:46:52 -0300

    chore(backend): handle already existent email response at create partner controller

--- backend/src/presentation/controllers/partner/CreatePartnerController.ts
+++ backend/src/presentation/controllers/partner/CreatePartnerController.ts
@@ -12,6 +12,9 @@ export class CreatePartnerController implements Controller {
     if (createPartnerResponse.type === 'INVALID_REQUEST')
       return HttpResponseHelper.badRequest(createPartnerResponse.message)
 
+    if (createPartnerResponse.type === 'EMAIL_ALREADY_EXISTS')
+      return HttpResponseHelper.badRequest('Email already exists')
+
     if (createPartnerResponse.type === 'ADDRESS_NOT_FOUND')
       return HttpResponseHelper.notFound('Address not found')
 
