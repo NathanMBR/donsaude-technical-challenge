fa891adbf4ca772beabc44c3b2cfd5f3456c5dd7
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-26 17:34:23 -0300

    chore(backend): check if email exists in create partner implementation

--- backend/src/data/usecases/partner/CreatePartnerImpl.ts
+++ backend/src/data/usecases/partner/CreatePartnerImpl.ts
@@ -1,6 +1,7 @@
 import { type CreatePartner } from '../../../domain'
 import {
   type CreatePartnerValidator,
+  type FindOnePartnerByEmailRepository,
   type FindOneAddressRepository,
   type HashService,
   type CreatePartnerRepository
@@ -9,6 +10,7 @@ import {
 export class CreatePartnerImpl implements CreatePartner {
   constructor (
     private readonly createPartnerValidator: CreatePartnerValidator,
+    private readonly findOnePartnerByEmailRepository: FindOnePartnerByEmailRepository,
     private readonly findOneAddressRepository: FindOneAddressRepository,
     private readonly hashService: HashService,
     private readonly createPartnerRepository: CreatePartnerRepository
@@ -20,6 +22,12 @@ export class CreatePartnerImpl implements CreatePartner {
       return {
         type: 'INVALID_REQUEST',
         message: validationResult.errorMessage
+      }
+
+    const partnerWithRequestedEmail = await this.findOnePartnerByEmailRepository.findOneByEmail({ email: validationResult.data.email })
+    if (partnerWithRequestedEmail)
+      return {
+        type: 'EMAIL_ALREADY_EXISTS'
       }
 
     const address = await this.findOneAddressRepository.findOne({ id: validationResult.data.addressId })
