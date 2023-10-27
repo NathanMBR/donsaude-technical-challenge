841af295b0f5bdb2913649e601350477bdffdded
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-27 15:08:11 -0300

    chore(frontend): make primary and secondary buttons centralized

--- frontend/src/components/layouts/PrimaryButton/PrimaryButton.tsx
+++ frontend/src/components/layouts/PrimaryButton/PrimaryButton.tsx
@@ -6,7 +6,7 @@ interface PrimaryButtonProps {
 
 export const PrimaryButton = ({ children, onClick }: PropsWithChildren<PrimaryButtonProps>) => (
   <button
-    className="px-4 py-2 w-full flex items-center gap-3 bg-primary hover:bg-primary-hover transition-colors duration-200 text-white rounded-2xl font-medium text-xs"
+    className="px-4 py-2.5 w-full flex items-center justify-center gap-3 bg-primary hover:bg-primary-hover transition-colors duration-200 text-white rounded-2xl font-bold text-xs"
     onClick={onClick}
   >
     {children}
--- frontend/src/components/layouts/SecondaryButton/SecondaryButton.tsx
+++ frontend/src/components/layouts/SecondaryButton/SecondaryButton.tsx
@@ -6,7 +6,7 @@ interface SecondaryButtonProps {
 
 export const SecondaryButton = ({ children, onClick }: PropsWithChildren<SecondaryButtonProps>) => (
   <button
-    className="px-4 py-2 w-full flex items-center gap-3 transition-colors duration-200 text-typography-dimmed hover:text-typography hover:bg-layout-button-hover-background rounded-2xl font-medium text-xs"
+    className="px-4 py-2.5 w-full flex items-center justify-center gap-3 transition-colors duration-200 text-typography-dimmed hover:text-typography hover:bg-layout-button-hover-background rounded-2xl font-bold text-xs"
     onClick={onClick}
   >
     {children}
