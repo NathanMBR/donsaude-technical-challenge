6cde3922774966cbe31d4d63b9689544a395940e
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-27 18:24:48 -0300

    chore(frontend): make input layout receive input register

--- frontend/src/components/layouts/Input/Input.tsx
+++ frontend/src/components/layouts/Input/Input.tsx
@@ -1,10 +1,30 @@
 import { InputHTMLAttributes } from "react"
+import { UseFormRegisterReturn } from "react-hook-form"
 
-export const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string
+  id: string,
+  errorMessage?: string
+  inputRegister?: UseFormRegisterReturn
+}
+
+export const Input = ({ label, id, placeholder = label, errorMessage, inputRegister, ...props }: InputProps) => {
   return (
-    <input
-      className="pl-10 py-2 flex items-center gap-3 border-solid border border-form-border text-form-field placeholder:text-form-field rounded-2xl text-xs"
-      {...props}
-    />
+    <div>
+      <label htmlFor={id}>{label}</label>
+      <input
+        className="px-3 w-full py-2 flex items-center gap-3 border-solid border border-form-border text-form-field placeholder:text-form-field rounded-2xl text-xs"
+        id={id}
+        name={id}
+        placeholder={placeholder}
+        {...props}
+        {...inputRegister}
+      />
+      {
+        errorMessage
+          ? <span className="text-red-500 text-sm">{errorMessage}</span>
+          : null
+      }
+    </div>
   )
 }
