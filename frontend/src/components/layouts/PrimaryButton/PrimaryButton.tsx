d3c32e572711139e6c461eecbd1eb6bbc07e02b5
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-27 11:31:00 -0300

    chore(frontend): add on click property to primary button layout

--- frontend/src/components/layouts/PrimaryButton/PrimaryButton.tsx
+++ frontend/src/components/layouts/PrimaryButton/PrimaryButton.tsx
@@ -1,7 +1,14 @@
 import { PropsWithChildren } from "react";
 
-export const PrimaryButton = ({ children }: PropsWithChildren) => (
-  <button className="px-4 py-2 w-full flex items-center gap-3 bg-primary hover:bg-primary-hover transition-colors duration-200 text-white rounded-2xl font-medium text-xs">
+interface PrimaryButtonProps {
+  onClick?: () => void
+}
+
+export const PrimaryButton = ({ children, onClick }: PropsWithChildren<PrimaryButtonProps>) => (
+  <button
+    className="px-4 py-2 w-full flex items-center gap-3 bg-primary hover:bg-primary-hover transition-colors duration-200 text-white rounded-2xl font-medium text-xs"
+    onClick={onClick}
+  >
     {children}
   </button>
 );
