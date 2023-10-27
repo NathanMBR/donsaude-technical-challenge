782bfbaf642b1a8d1573d510422c5bcf924554c7
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-27 10:30:36 -0300

    refactor(frontend): separate new service button at header component

--- frontend/src/components/layouts/Header/Header.tsx
+++ frontend/src/components/layouts/Header/Header.tsx
@@ -1,13 +1,25 @@
+import { MagnifyingGlass, PlusCircle } from "@phosphor-icons/react"
+
+const NewServiceButton = () => {
+  return (
+    <button className="px-4 py-2 flex items-center gap-3 bg-primary-light hover:text-layout-button-hover-text transition-colors duration-200 text-primary rounded-2xl font-semibold text-xs">
+      <div>
+        <PlusCircle />
+      </div>
+      <span>Novo atendimento</span>
+    </button>
+  )
+}
+
 export const Header = () => {
   return (
     <header className="flex justify-between px-12 py-6">
       <div className="flex gap-4">
-        <button className="px-4 py-2 flex items-center gap-3 bg-primary-light hover:text-layout-button-hover-text transition-colors duration-200 text-primary rounded-2xl font-semibold text-xs">
-          <div className="w-5 h-5 bg-primary"></div>
-          <span>Novo atendimento</span>
-        </button>
+        <NewServiceButton />
 
-        <div className="relative">
+        <div className="relative flex items-center">
+          <MagnifyingGlass className="absolute ml-3" />
+
           <input className="pl-10 py-2 flex items-center gap-3 border-solid border border-form-border text-form-field placeholder:text-form-field rounded-2xl text-xs" placeholder="Buscar usuário" />
         </div>
       </div>
