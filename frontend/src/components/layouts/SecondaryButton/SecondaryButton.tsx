80c6427a860168c08800012e7ca97e538a2348c0
Nathan Murillo <nathanmurillodeoliveira@gmail.com>
2023-10-27 11:33:13 -0300

    chore(frontend): add on click property to secondary button layout

--- frontend/src/components/layouts/SecondaryButton/SecondaryButton.tsx
+++ frontend/src/components/layouts/SecondaryButton/SecondaryButton.tsx
@@ -1,7 +1,14 @@
 import { PropsWithChildren } from "react";
 
-export const SecondaryButton = ({ children }: PropsWithChildren) => (
-  <button className="px-4 py-2 w-full flex items-center gap-3 transition-colors duration-200 text-typography-dimmed hover:text-typography hover:bg-layout-button-hover-background rounded-2xl font-medium text-xs">
+interface SecondaryButtonProps {
+  onClick?: () => void
+}
+
+export const SecondaryButton = ({ children, onClick }: PropsWithChildren<SecondaryButtonProps>) => (
+  <button
+    className="px-4 py-2 w-full flex items-center gap-3 transition-colors duration-200 text-typography-dimmed hover:text-typography hover:bg-layout-button-hover-background rounded-2xl font-medium text-xs"
+    onClick={onClick}
+  >
     {children}
   </button>
 )
